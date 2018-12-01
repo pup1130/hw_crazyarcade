1cafbd37ed772ef0565b7377d3665c9e3ec466ab
String137 <38708108+String137@users.noreply.github.com>
2018-12-01 13:28:09 +0900

    Update Constant.java

--- src/crazyarcade/Constant.java
+++ src/crazyarcade/Constant.java
@@ -2,13 +2,20 @@ package crazyarcade;
 
 public interface Constant {
 
-    int SCREEN_WIDTH = 960;
-    int SCREEN_HEIGHT = 595;
-    int START_BUTTON_WIDTH = 240;
-    int START_BUTTON_HEIGHT = 120;
-    int MENU_BUTTON_LENGTH = 200;
-    int RETURN_START_BUTTON_LENGTH = 50;
+    final int SCREEN_WIDTH = 960;
+    final int SCREEN_HEIGHT = 595;
+    final int START_BUTTON_WIDTH = 240;
+    final int START_BUTTON_HEIGHT = 120;
+    final int MENU_BUTTON_LENGTH = 200;
+    final int RETURN_START_BUTTON_LENGTH = 50;
 
+	final int BLOCK_COUNT = 15;
+	final int ONE_BLOCK_LENGTH = 36;
+	final int Y_RELAX = 2;
+	final int X_RELAX = 2;
+	final int CAT_HEIGHT = 24;
+	final int MAP_SIZE = 540;
+	final int WOOL_RELAX = 2;
 
     String GAME_VERSION = "Dev.";
 
