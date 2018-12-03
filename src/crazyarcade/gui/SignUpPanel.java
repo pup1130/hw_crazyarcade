c150eae8e8de85479fdf12733444685ef03263af
String137 <38708108+String137@users.noreply.github.com>
2018-12-03 11:30:41 +0900

    Create SignUpPanel.java

---
+++ src/crazyarcade/gui/SignUpPanel.java
@@ -0,0 +1,14 @@
+package crazyarcade.gui;
+
+import crazyarcade.Constant;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SignUpPanel extends JPanel implements Constant {
+	SignUpPanel(){
+		setVisible(true);
+		setLayout(null);
+
+	}
+}
