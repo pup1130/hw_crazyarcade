ac975e274a251500eae4cf4f403576ad98264524
String137 <38708108+String137@users.noreply.github.com>
2018-12-03 11:30:04 +0900

    Create SignInPanel.java

---
+++ src/crazyarcade/gui/SignInPanel.java
@@ -0,0 +1,46 @@
+package crazyarcade.gui;
+
+import crazyarcade.Constant;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SignInPanel extends JPanel implements Constant {
+
+    class BackgroundPanel extends JPanel{
+        private Image image;
+
+        BackgroundPanel(Image image) {
+            this.image = image;
+            setSize(new Dimension(image.getWidth(null), image.getHeight(null)));
+            setLayout(null);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            g.drawImage(image, 0, 0, null);
+        }
+    }
+
+    SignInPanel() {
+        setVisible(true);
+        setLayout(null);
+
+        BackgroundPanel BackgroundPanel = new BackgroundPanel(
+                new ImageIcon("src\\crazyarcade\\gui\\startBackground.png").getImage());
+        add(BackgroundPanel);
+
+        JButton okButton = new JButton(new ImageIcon("src\\crazyarcade\\gui\\ok.png"));
+        okButton.setBorderPainted(false);
+        okButton.setContentAreaFilled(false);
+        okButton.setFocusPainted(false);
+
+        okButton.addActionListener(e -> Frame.cards.show(getParent(), "StartPanel"));
+
+        okButton.setBounds(SCREEN_WIDTH / 2 - START_BUTTON_WIDTH / 2, (SCREEN_HEIGHT - 55) / 2 - START_BUTTON_HEIGHT / 2, START_BUTTON_WIDTH, START_BUTTON_HEIGHT);
+
+        BackgroundPanel.add(okButton);
+    }
+
+}
